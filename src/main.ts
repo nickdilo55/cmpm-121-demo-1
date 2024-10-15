f98871d3f98e4cb0d36ded5545236e9e8760aa2d
nickdilo55 <ndiloren@ucsc.edu>
2024-10-15 21:19:11 +0000

    Pineapple

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.addEventListener("click", () => {
+    alert("P I N E A P P L E");
+});
+
+app.appendChild(button);

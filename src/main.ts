790031c689a4ed35500f971a277fb38764a6f167
nickdilo55 <ndiloren@ucsc.edu>
2024-10-15 21:11:09 +0000

    Name Change

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Flicker Clicker";
+const gameName = "Pineapple";
 document.title = gameName;
 
 const header = document.createElement("h1");

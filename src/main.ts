6ef3d73772c9dbfd1191486130dbcc074d55c824
nickdilo55 <ndiloren@ucsc.edu>
2024-10-15 21:38:01 +0000

    Counter

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,20 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+const count = document.createElement("div");
+let counter: number = 0;
+
+const updateCount = () => {
+    count.textContent = `${counter} pineapples eaten`;
+};
+
+updateCount();
+app.append(count);
+
 const button = document.createElement("button");
 button.textContent = "🍍";
+button.addEventListener("click", () => {
+  counter++;
+  updateCount();
+});
 app.appendChild(button);

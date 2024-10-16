2768157b370a7582df629fac9149c7f5993b805d
nickdilo55 <ndiloren@ucsc.edu>
2024-10-16 20:38:08 +0000

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,10 @@ app.append(header);
 
 const count = document.createElement("div");
 let counter: number = 0;
+let growth: number = 0;
 
 const updateCount = () => {
-  count.textContent = `${Math.floor(counter)} pineapples eaten`;
+  count.textContent = `${Math.floor(counter)} pineapples stolen`;
 };
 
 updateCount();
@@ -24,16 +25,44 @@ button.textContent = "🍍";
 button.addEventListener("click", () => {
   counter++;
   updateCount();
+  upgradeButtonVisible();
 });
 app.appendChild(button);
+
+const upgrades = document.createElement("button");
+upgrades.textContent = "Increase Growth (COST: 10 Pineapples)";
+upgrades.disabled = true;
+upgrades.style.color = "grey";
+
+const upgradeButtonVisible = () => {
+  if (counter >= 10) {
+    upgrades.disabled = false;
+    upgrades.style.color = "white";
+  }
+  else {
+    upgrades.disabled = true;
+    upgrades.style.color = "grey";
+  }
+}
+
+upgrades.addEventListener("click", () => {
+  if (counter >= 10) {
+    counter -= 10;
+    growth += 1;
+    updateCount();
+    upgradeButtonVisible();
+  }
+});
+
+app.append(upgrades);
 
 let zero = performance.now();
 
 const updateCounter = (curr: number) => {
-    counter += (curr - zero) / 1000;
-    updateCount();
-
-    zero = curr;
-    requestAnimationFrame(updateCounter);
-}
+  counter += ((curr - zero) / 1000) * growth;
+  updateCount();
+  upgradeButtonVisible();
+  zero = curr;
+  requestAnimationFrame(updateCounter);
+};
 requestAnimationFrame(updateCounter);
